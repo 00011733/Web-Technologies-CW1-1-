018aa3cb38f37ed69aa5ff8a03c1f7004446e39f
00011733 <00011733wiut@gmail.com>
2021-12-09 20:57:16 +0500

    Login form

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 // ELEMENTS
 const menuEl = document.getElementById("menu");
 const navbarEl = document.querySelector(".navbar");
+const loginEl = document.querySelector("#login");
+const loginFormEl = document.querySelector(".login-form");
+const xEl = document.querySelector('.login-form form .fa-times');
 
 // Menu navbar
 menuEl.addEventListener('click', () => {
     menuEl.classList.toggle('fa-times');
     navbarEl.classList.toggle("nav-toggle");
 })
+
+// LOGIN form
+loginEl.addEventListener('click', function() {
+    loginFormEl.classList.add('popup');
+});
+
+xEl.addEventListener('click', function () {
+    loginFormEl.classList.remove('popup');
+});
+
+
 
 
 
